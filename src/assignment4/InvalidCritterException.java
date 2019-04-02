543dc76170c4d484ce35ac9f467b033f11317454
11irisham <31528956+11irisham@users.noreply.github.com>
2019-04-01 19:15:02 -0500

    added header

--- src/assignment4/InvalidCritterException.java
+++ src/assignment4/InvalidCritterException.java
@@ -1,5 +1,17 @@
 package assignment4;
 
+/* CRITTERS InvalidCritterException.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Sam Wang
+ * sjw2752
+ * 16215
+ * Iris Ham
+ * ih4548
+ * 16215
+ * Slip days used: <0>
+ * Spring 2019
+ */
 
 public class InvalidCritterException extends Exception {
 	String offending_class;
