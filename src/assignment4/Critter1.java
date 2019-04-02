9a6ac70852fac241f87887dba5e611ed5ad47dab
11irisham <31528956+11irisham@users.noreply.github.com>
2019-04-01 19:03:01 -0500

    added header

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -1,5 +1,19 @@
 package assignment4;
 
+/* CRITTERS Critter.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Sam Wang
+ * sjw2752
+ * 16215
+ * Iris Ham
+ * ih4548
+ * 16215
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+
 public class Critter1 extends Critter {
     // Gambler Critter
     // fights if energy is <= half energy
